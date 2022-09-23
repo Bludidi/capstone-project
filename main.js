7bc3673326e105be69bac2ac5991066524edf7b6
Bludidi <bludidi5@gmail.com>
2022-09-23 04:01:30 +0200

    added js file

---
+++ main.js
@@ -0,0 +1,93 @@
+// menu display 
+
+const menu = document.querySelector('.menu');
+const menuOption = document.querySelector('.menu_opt');
+const closeMenu = document.getElementById('exit');
+const menuItems = document.querySelector('.menu_list');
+
+function show() {
+  menuOption.style.display = 'block';
+  menuOption.classList.toggle('open');
+  menu.style.visibility = 'hidden';
+}
+
+function close() {
+  menuOption.style.display = 'none';
+  menuOption.classList.remove('open');
+  menu.style.visibility = 'visible';
+}
+
+menu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+menuItems.addEventListener('click', close);
+
+// Artits section 
+
+const artists = [
+  {
+    name: 'Erykah Badu',
+    bio: 'American singer-songwriter, record producer and actress.',
+    message: 'Erykah will be showcasing her talent in this event',
+    image: './assets/images/Erykah_Badu.jpg',
+  },
+
+  {
+    name: 'DJ Khaled',
+    bio: 'Khaled Mohammed Khaled, known professionally as DJ Khaled, is an American DJ, record executive, author and record producer.',
+    message: 'This prominent DJ will be showcasing his talent in this event',
+    image: './assets/images/Dj_Khaled2.jpg',
+  },
+
+  {
+    name: 'Ali Campbell',
+    bio: 'Alistair Ian Campbell (born 15 February 1959) is an English singer and songwriter who was the lead singer and a member of the British reggae band UB40.',
+    message: 'A vetaran in the musical fratenity will be also sharing his artistic skills',
+    image: './assets/images/Ali_Campbell.jpg',
+  },
+
+  {
+    name: 'Lauryn Hill',
+    bio: 'Lauryn Noelle Hill (born May 26, 1975) is an American rapper, singer, songwriter, and actress. She is often regarded as one of the greatest rappers of all time,[2] as well as being one of the most influential musicians of her generation.',
+    message: 'Lauryn will be showcasing her great talent in this event',
+    image: './assets/images/lauryn_hill2.jpg',
+  },
+
+  {
+    name: 'Black Coffee',
+    bio: 'Nkosinathi Innocent Maphumulo is a South African DJ, record producer and a songwriter. Black Coffee, as he is known is also a Grammy Award winner and a prominent figure.',
+    message: 'Black Coffee will be showcasing his great talent in this event',
+    image: './assets/images/black_coffee2.jpg',
+  },
+
+  {
+    name: 'Adele',
+    bio: 'Adele Laurie Blue Adkins MBE is an English singer and songwriter',
+    message: 'Adele will be showcasing her great talent in this event',
+    image: './assets/images/adele.jpg',
+  },
+]; 
+
+// projecting artist information into html
+
+const artistCards = artists.map((artist) => `
+<div id="card1" class="cards">
+            <div id="card_img" class="card left_align"><img class="card_img" src="${artist.image}" alt="artist card"></div>
+              <div id="txt_img" class="artist-name"><h3>${artist.name}</h3>
+                <p class="p1">${artist.bio}</p>
+                <br>
+                <p class="p2">${artist.message}</p>
+            </div>
+        </div>
+`);
+
+// adjusting image sizes
+
+// const artistImage = artists.image;
+
+
+const cardSection = document.querySelector('.artists');
+const parser = new DOMParser();
+artistCards.forEach((artistCard, index) => {
+  const artistIndex = parser.parseFromString(artistCard, 'text/html').body.firstChild;
+  cardSection.append(artistIndex);
+});
